2de02656e65fbf4005d2ab77bc8e941a252ea2cb
Panos Paganis <mallinanga@gmail.com>
2014-11-03 19:53:26 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ var main = new UI.Menu({
             //icon: 'images/menu_icon.png',
             //subtitle: '...'
         }, {
+            title: 'Coffee+1'
+            //icon: 'images/menu_icon.png',
+            //subtitle: '...'
+        }, {
             title: 'Phones'
             //icon: 'images/menu_icon.png',
             //subtitle: 'Can do Menus'
@@ -33,14 +37,11 @@ var main = new UI.Menu({
     }]
 });
 main.on('select', function (e) {
-    console.log(e);
-    console.log(JSON.stringify(e));
     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
     console.log('The item is titled "' + e.item.title + '"');
     ajax({url: 'http://kul-phones.herokuapp.com/api/users/' + e.itemIndex, type: 'json'},
         function (data) {
             console.log(data.data.first_name + ' ' + data.data.last_name);
-            //main.item.subtitle(data.data.first_name + ' ' + data.data.last_name);
         });
 });
 main.show();
