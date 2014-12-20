90318bb67f77d365c76cdf2f127a386fc5efa6ad
Panos Paganis <mallinanga@gmail.com>
2014-12-20 11:06:41 +0200

    Update index.html main.js stylesheet.css

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,3 @@
-//console.log('This would be the main JS file.');
 function saveOptions() {
     var options = {};
     $('textarea, select, [type="hidden"], [type="password"], [type="text"]').each(function () {
@@ -11,19 +10,15 @@ function saveOptions() {
 }
 $().ready(function () {
     $("#b-cancel").click(function () {
-        console.log("Cancel");
         document.location = "pebblejs://close";
     });
     $("#b-submit").click(function () {
-        console.log("Save");
         var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(saveOptions()));
-        console.log("Warping to: " + location);
         console.log(location);
         document.location = location;
     });
-    //var obj = jQuery.parseJSON(decodeURIComponent(window.location.search.substring(1)));
-    //for (key in obj) {
-    //    $("#" + [key]).val(obj[key]);
-    //    $("#" + [key]).val(obj[key]).slider("refresh");
-    //}
+    var obj = jQuery.parseJSON(decodeURIComponent(window.location.search.substring(1)));
+    for (key in obj) {
+        $("#" + [key]).val(obj[key]);
+    }
 });
